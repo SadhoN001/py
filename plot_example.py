de99d99cd33d1dde807e95f330a1eb6863a65f9a
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-05-21 20:55:20 +0600

    Updated plot_example.py

--- plot_example.py
+++ plot_example.py
@@ -194,3 +194,13 @@ plt.title('exam_score Histogram')
 plt.xlabel('exam_score')
 plt.ylabel('frequency')
 plt.show()
+
+# Pie Charts-------------------------------------------------->
+import matplotlib.pyplot as plt
+import numpy as np
+
+y= np.array([35,25,25,15])
+mylabels=['apple','banana','orange','dates']
+plt.pie(y, labels=mylabels)
+plt.legend()
+plt.show()
