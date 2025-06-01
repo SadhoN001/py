6acdf5b92b6ddd9e2f8326544a4405ff836b1a93
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-06-01 20:54:03 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
 ## pip install mysql-connector-python
-import mysql.connector
+import mysql.connector #Imports the module needed to interact with a MySQL database from Python.
 
 mydb= mysql.connector.connect(
     host="localhost",
-    user=" root",
+    user="root",
     password='Sadhon123#',
     # database='sadhondb'
 )
 
-mycursor= mydb.cursor()
+mycursor= mydb.cursor() # A cursor allows you to execute SQL commands on the database connection.
 # mycursor.execute("CREATE DATABASE sadhondb")
 mycursor.execute("SHOW DATABASES")
 
