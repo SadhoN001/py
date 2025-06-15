26da3c074c7a4a0d72fe6178eb230b6a5c48ec7e
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-06-15 12:43:57 +0600

    Updated

--- day5(oop,solid).py
+++ day5(oop,solid).py
@@ -368,6 +368,78 @@ print(f._age)
 # b=rectangle()
 # print(b.area(5,4))
 
+### Singleton Design pattern--------------------->
+
+class Singleton:
+    _instance = None #class variable
+    
+    def __new__(cls):
+        if cls._instance is None:
+            print("1st object is created")
+            cls._instance= super(Singleton, cls).__new__(cls)
+        return cls._instance
+            
+ob1= Singleton()
+ob2= Singleton()
+print(ob1 is ob2)
+
+### Factory Design pattern--------------------->
+class Car:
+    def driver(self):
+        return "Driving a car"
+    
+class Bike:
+    def driver(self):
+        return "Riding a bike"
+    
+class VehicalFactory:
+    @staticmethod
+    def get_vehical(type):
+        if type == "car":
+            return Car()
+        elif type == "bike":
+            return Bike()
+        else:
+            return ValueError("Unkown bike")
+
+vehical= VehicalFactory.get_vehical("car")
+print(vehical.driver())
+
+### Builder Design pattern--------------------->
+class Computer:
+    def __init__(self,cpu, ram, storage):
+        self.cpu= cpu
+        self.ram= ram
+        self.storage= storage
+
+    def __str__(self):
+        return f"Computer with {self.cpu} CPU, {self.ram} RAM, {self.storage} Storage"
+    
+class ComputerBuilder:
+    def __init__(self):
+        self.cpu= None
+        self.ram= None
+        self.storage= None
+        
+    def set_cpu(self, cpu):
+        self.cpu=cpu
+        return self
+    
+    def set_ram(self, ram):
+        self.ram=ram
+        return self
+    
+    def set_storage(self, storage):
+        self.storage=storage
+        return self
+        
+    def build(self):
+        return Computer(self.cpu, self.ram, self.storage)
+    
+builder= ComputerBuilder()
+computer = builder.set_cpu("intel i9").set_ram("32GB").set_storage("1TB SSD").build()
+print(computer)
+
 # Single Responsibility Principle (SRP).--------------------------->
 # # oop part-1 12number slid task 3 
 class Car:
