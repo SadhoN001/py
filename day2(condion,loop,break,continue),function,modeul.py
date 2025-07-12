0e5e0b6116ade06265f7515527583addd771bcc3
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-07-12 20:24:58 +0600

    Updated

--- day2(condion,loop,break,continue),function,modeul.py
+++ day2(condion,loop,break,continue),function,modeul.py
@@ -3,6 +3,9 @@ formated_pi= "value of pi:{:.4f}".format(pi)
 #formated_pi= round(pi,4)
 print(formated_pi)
 
+# a=5
+# print(f"{a:04d}")
+
 print('\u21AA')
 
 # # Bitwaise-------------------------->
