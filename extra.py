132ea6aa69bfe0201291d91258cc2017213c72d4
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-08-05 16:24:17 +0600

    Create extra.py

---
+++ extra.py
@@ -0,0 +1,45 @@
+# a="hello world"
+# print(a)
+# print(a[1])
+# print(a[1:4])
+# print(a[6:-1])
+
+# a="   hello world"
+# print(a)
+# print(a.strip())
+# a=5
+# loc=id(a)
+# a=6
+# loc1=id(a)
+# print(loc)
+# print(loc1)
+# name= """he 'said'
+# "hello world"
+# """
+# print(name)
+
+# n= input()
+# a,b,c=map(int,input().split())
+# print(a,b,c*9)
+
+# a="hello"
+# b="world"
+# k="skd"
+# c="".join([a,b,k])+"!"
+# print(c)
+# c="{} {} {}".format(a,b,k)
+# print(c)
+# c="%s %s %s" %(a,b,k)
+# print(c)
+
+# a=[1,2,3,4,5]
+# num_itr= iter(a)
+# print(next(num_itr))
+# print(next(num_itr))
+# print(next(num_itr))
+# print(next(num_itr))
+# print(next(num_itr))
+
+
+
+
