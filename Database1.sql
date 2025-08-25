d60a0c0d2ded4fc52ba3fe480bdbc08df7bbb7f2
Sadhon Kumar Dev <skdsadhon@gmail.com>
2025-08-25 18:59:44 +0600

    Update Database1.sql

--- Database1.sql
+++ Database1.sql
@@ -178,3 +178,10 @@ SELECT DISTINCT CITY
 FROM STATION 
 WHERE CITY NOT REGEXP '^[aeiou]' AND CITY NOT REGEXP '[aeiou]$'
 
+-- Order your output by the last three characters of each name.
+-- If two or more students both have names ending in the same
+-- last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending
+SELECT NAME FROM STUDENTS
+WHERE MARKS>75 
+ORDER BY RIGHT(NAME,3), ID;
+
